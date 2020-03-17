4dae0dc44a191fce9255cdeb76e5c1a6c1589859
Madhav Pokhrel <saraswatiacharya05@gmail.com>
2020-03-17 18:20:27 -0400

    added New project field

--- finder-front-end/src/App.js
+++ finder-front-end/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Users from './components/Users'
+import Projects from './components/Projects'
 
 function App() {
   return (
     <div>
       <h1>Programmer Finder Application! </h1>
       <Users/>
+      <Projects/>
     </div>
   )
 }
