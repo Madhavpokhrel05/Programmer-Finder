7c1771fc75c5af505a03be337664a6d84e10f799
Madhav Pokhrel <saraswatiacharya05@gmail.com>
2020-03-17 15:46:32 -0400

    added new user page

--- finder-back-end/server.js
+++ finder-back-end/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = 3003
-<<<<<<< HEAD
+// <<<<<<< HEAD
 const projects = require('./controllers/projects.js')
-=======
+// =======
 const projectController = require('./controllers/projects.js')
->>>>>>> a33a70be516b1dc04636271d7bcaef869784ae35
+// >>>>>>> a33a70be516b1dc04636271d7bcaef869784ae35
 const mongoose = require('mongoose')
 const cors = require('cors')
 
