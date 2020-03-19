920fdf5ae99af9775031e65ebc2fa637271419bf
Alfonso Garcia <ajgarcia321@gmail.com>
2020-03-19 10:10:57 -0700

    Deleted a lot of unnecessary files in order to clean up our program

--- finder-front-end/src/App.js
+++ finder-front-end/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Users from './components/Users'
 import Projects from './components/Projects'
+
 function App() {
   return (
     <div>
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-md-8">
-        <h1 className="heading-title">Programmer Finder Application! </h1>
-        <Users/>
+        <h1 className="heading-title">Program Manager!</h1>
         <Projects/>
-
         </div>
       </div>
       </div>
