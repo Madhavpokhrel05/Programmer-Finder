77a2a2e04e03c7a18a7ed2165773130986f50c4e
Madhav Pokhrel <saraswatiacharya05@gmail.com>
2020-03-19 13:22:08 -0400

    pull from manager.js

--- finder-front-end/src/components/NewProject.js
+++ finder-front-end/src/components/NewProject.js
@@ -21,46 +21,38 @@ class App extends React.Component{
     return(
       <div>
         <form onSubmit={(event)=>this.props.handleAddProject(event,this.state)}>
-
-        <div className="input-group mb-3">
-          <input type="text"   placeholder="Enter Project Name"
-          name="name"
-          value={this.state.name}
-          onChange={this.handleChange} className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" />
-          <div className="input-group-append">
+          <div className="input-group mb-3">
+            <input type="text"   placeholder="Enter Project Name"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange} className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" />
+            <div className="input-group-append">
             <span className="input-group-text" id="basic-addon2"><i className="fa fa-object-ungroup"></i></span>
           </div>
         </div>
 
           <div className="input-group mb-3">
             <input type="text"
-            placeholder="Enter Project URL"
-            name="url"
-            value={this.state.url}
-            onChange={this.handleChange}
-             className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" />
+              placeholder="Enter Project URL"
+              name="url"
+              value={this.state.url}
+              onChange={this.handleChange}
+              className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" />
             <div className="input-group-append">
               <span className="input-group-text" id="basic-addon2"><i className="fa fa-link"></i></span>
             </div>
           </div>
 
-
-
           <div className="input-group mb-3">
             <textarea  rows="10"
-
-            placeholder="Enter Project Description"
-            name="description"
-            value={this.state.description}
-            onChange={this.handleChange}
-
-             className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" ></textarea>
-
+              placeholder="Enter Project Description"
+              name="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+              className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" ></textarea>
           </div>
 
-
           <input type="submit" value="submit" className="btn btn-info text-uppercase" />
-
         </form>
       </div>
     )
--- finder-front-end/src/components/NewUser.js
+++ finder-front-end/src/components/NewUser.js
@@ -21,36 +21,32 @@ class App extends React.Component{
     return(
       <div>
         <form onSubmit={(event)=>this.props.handleAddUser(event,this.state)}>
-
-
           <div className="input-group mb-3">
             <input type="text"   placeholder="Enter Username"
               name="username"
               value={this.state.username}
               onChange={this.handleChange} className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" />
-            <div className="input-group-append">
+              <div className="input-group-append">
               <span className="input-group-text" id="basic-addon2"><i className="fa fa-id-card"></i></span>
             </div>
           </div>
 
           <div className="input-group mb-3">
             <input type="text"   placeholder="Enter Password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange} className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" />
-            <div className="input-group-append">
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange} className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" />
+              <div className="input-group-append">
               <span className="input-group-text" id="basic-addon2"><i className="fa fa-lock"></i></span>
             </div>
           </div>
 
           <div className="input-group mb-3">
             <textarea  rows="10"  placeholder="Project Description"
-            name="projects"
-            value={this.state.projects}
-            onChange={this.handleChange} className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" ></textarea>
-
+              name="projects"
+              value={this.state.projects}
+              onChange={this.handleChange} className="form-control"  aria-label="Recipient's username" aria-describedby="basic-addon2" ></textarea>
           </div>
-
 
           <input type="submit" value="submit" className="btn btn-info text-uppercase" />
         </form>
